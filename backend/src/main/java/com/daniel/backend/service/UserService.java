adcd0f34de0e5bdc3185f7393068f5fed2a90029
Daniel <jamoja64749@gmail.com>
2025-05-31 19:56:01 +0200

    Initial commit: Set up monorepo

---
+++ backend/src/main/java/com/daniel/backend/service/UserService.java
@@ -0,0 +1,24 @@
+package com.daniel.backend.service;
+
+import com.daniel.backend.model.User;
+import com.daniel.backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+}

58148e093ff1e60ad75ac9345992571b3675ddf2
Daniel <danczer63@edu.gdansk.pl>
2025-05-31 19:56:01 +0200

    Initial commit: Set up monorepo

---
+++ backend/src/main/java/com/daniel/backend/dto/RegisterUserDto.java
@@ -0,0 +1,13 @@
+package com.daniel.backend.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterUserDto {
+    private String email;
+    private String password;
+    private String username;
+}
